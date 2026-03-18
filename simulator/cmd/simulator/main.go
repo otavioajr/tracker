2f42eef4ee7c587bab0b66932450b98fec3a7878
Otavio Junior <otavioajr@gmail.com>
2026-03-18 07:32:35 -0300

    chore: init device simulator module

---
+++ simulator/cmd/simulator/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("tracker device simulator")
+	fmt.Println("usage: simulator --host <gateway-host> --port <gateway-port> --imei <device-imei>")
+}
---
+++ simulator/go.mod
@@ -0,0 +1,3 @@
+module github.com/otavioajr/tracker/simulator
+
+go 1.26.1
